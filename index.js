bf7a809fd05e839fc58999d72c6ce2607985f0e9
jayesh <jayeshsarvaiya9913@gamil.com>
2024-09-05 17:04:44 +0530

    add .env

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
-import cor from "cors";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({});
 const app = express();
 app.use(cors());
 const port = 3000;
--- model.js
+++ model.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config({});
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/odles");
+    await mongoose.connect(process.env.MONGO);
     console.log("connected");
   } catch (error) {
     console.log(error);
